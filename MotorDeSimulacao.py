10b4908f10e477ad5932fa81ac713bc7ce6f497f
Gonçalo Moita <gcnma@iscte-iul.pt>
2025-11-21 01:07:41 +0000

    uml criado e classes ambiente

--- MotorDeSimulacao.py
+++ MotorDeSimulacao.py
@@ -55,7 +55,7 @@ class MotorDeSimulacao:
         for passo in range(self.passos):
             for agente in self.listaAgentes():
                 self.ambiente.observacaoPara(agente)
-                # print(f"Agente {agente.nome} na posicao ({agente.x}, {agente.y}) com observacao: {agente.observacaoCurrente}")#eleminar futuramente esta parte
+                print(f"Agente {agente.nome} na posicao ({agente.x}, {agente.y}) com observacao: {agente.observacaoCurrente}")#eleminar futuramente esta parte
                 accao = agente.age()
                 self.ambiente.agir(accao, agente)
                 if(agente.x, agente.y) == self.ambiente.farol: #eleminar futuramente esta parte
@@ -67,14 +67,14 @@ class MotorDeSimulacao:
                 time.sleep(1)
 
     def drawingWorld(self):
-        world = [["." for _ in range(self.ambiente.sizeX)] for _ in range(self.ambiente.sizeY)]
+        world = [[" . " for _ in range(self.ambiente.sizeX)] for _ in range(self.ambiente.sizeY)]
         if self.ambiente.farol is not None:
             fx, fy = self.ambiente.farol
-            world[fy][fx] = "T"
+            world[fy][fx] = " T "
         for agente in self.listaAgentes():
-            world[agente.y][agente.x] = agente.nome
+            world[agente.y][agente.x] = f" {agente.nome} "
         for obstaculo in self.ambiente.obstaculos:
-            world[obstaculo[1]][obstaculo[0]] = "#"
+            world[obstaculo[1]][obstaculo[0]] = " # "
         for w in world:
             print("".join(w))
         print()
